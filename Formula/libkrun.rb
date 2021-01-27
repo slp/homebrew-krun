20debcc2a45857668feeb2fd9e5769ed7cf77e38
Sergio Lopez <slp@sinrega.org>
2021-01-27 15:49:39 +0100

    libkrun: update 0.1.3 bottle.

--- Formula/libkrun.rb
+++ Formula/libkrun.rb
@@ -5,6 +5,12 @@ class Libkrun < Formula
   sha256 "a8d5d2d7f631ba42b25a7b977349e8856b9d025e405afc50fd0ba992671afce5"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://dl.bintray.com/slp/bottles-krun"
+    cellar :any
+    sha256 "b8a811eaa7bf07e826c20a4e502ae2a0bbd1aa6000af21278b31af5325786a40" => :arm64_big_sur
+  end
+
   depends_on "rust" => :build
   depends_on "dtc"
   depends_on "libkrunfw"
