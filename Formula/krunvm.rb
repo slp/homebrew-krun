fc5147ea695f244d6865ffede3d25512e81f6965
Sergio Lopez <slp@sinrega.org>
2021-01-27 15:49:39 +0100

    krunvm: update 0.1.0 bottle.

--- Formula/krunvm.rb
+++ Formula/krunvm.rb
@@ -5,6 +5,12 @@ class Krunvm < Formula
   sha256 "c53a2264144f0c09dbc61a9953225d82289951000d83f9dbf67f1b3b9b48d4b8"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://dl.bintray.com/slp/bottles-krun"
+    cellar :any
+    sha256 "22a4667f0d1053aa18c64dcbc363721ff16dbefa789ca5a41832ff9a66c40815" => :arm64_big_sur
+  end
+
   depends_on "rust" => :build
   depends_on "buildah"
   depends_on "libkrun"
