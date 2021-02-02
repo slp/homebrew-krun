db190f71cfd143857f89bf0645267ba585afbf09
Sergio Lopez <slp@sinrega.org>
2021-02-02 18:52:17 +0100

    krunvm: Update formula to 0.1.4

--- Formula/krunvm.rb
+++ Formula/krunvm.rb
@@ -1,8 +1,8 @@
 class Krunvm < Formula
   desc "Manage lightweight VMs created from OCI images"
   homepage "https://github.com/slp/krunvm"
-  url "https://github.com/slp/krunvm/archive/v0.1.0.tar.gz"
-  sha256 "c53a2264144f0c09dbc61a9953225d82289951000d83f9dbf67f1b3b9b48d4b8"
+  url "https://github.com/slp/krunvm/archive/v0.1.4.tar.gz"
+  sha256 "0ed02ff10a04e40431b50121f292b0b6b4b625ca6a12196811cb5ebbba0ad53d"
   license "Apache-2.0"
 
   bottle do
