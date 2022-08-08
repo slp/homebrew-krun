949c553abe99041fbf6c8ce355b322174e0891a4
Sergio Lopez <slp@sinrega.org>
2022-08-08 21:39:46 +0200

    libkrun: add bottle for v1.3.0

--- Formula/libkrun.rb
+++ Formula/libkrun.rb
@@ -5,6 +5,11 @@ class Libkrun < Formula
   sha256 "441a9e194841e9276df89ba3c90056ffbea66709d3d0b36cf40318afcf8e0955"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://raw.githubusercontent.com/slp/homebrew-krun/master/bottles"
+    sha256 cellar: :any, arm64_monterey: "be1ae678c25ef669adbaf3a23afbb04ec37912581b6f2ba245eae8a87cffc107"
+  end
+
   depends_on "rust" => :build
   # Upstream only supports Hypervisor.framework on arm64
   depends_on arch: :arm64
