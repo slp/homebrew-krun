2f53533aacfebabe9032bf4af59c24dd4db5921f
Sergio Lopez <slp@sinrega.org>
2022-11-07 17:59:29 +0100

    libkrun: Update to version 3.8.1

--- Formula/libkrunfw.rb
+++ Formula/libkrunfw.rb
@@ -1,13 +1,13 @@
 class Libkrunfw < Formula
   desc "Dynamic library bundling a Linux kernel in a convenient storage format"
   homepage "https://github.com/slp/libkrunfw"
-  url "https://github.com/containers/libkrunfw/releases/download/v3.8.0/v3.8.0-with_macos_prebuilts.tar.gz"
-  sha256 "c72292e28257a4071dbbd20edbaaed024381a221ba665154e3225181ff73dd88"
+  url "https://github.com/containers/libkrunfw/releases/download/v3.8.1/v3.8.1-with_macos_prebuilts.tar.gz"
+  sha256 "8bb6ed8c60606fdded1ec8484b4d91a78f76881e1a1b437852e470bfa3e4ba48"
   license all_of: ["GPL-2.0-only", "LGPL-2.1-only"]
 
   bottle do
     root_url "https://raw.githubusercontent.com/slp/homebrew-krun/master/bottles"
-    sha256 cellar: :any, arm64_ventura: "3aa9afdc4f0af9e186955ddbe1831279a0d358ebd3b9b9f2ad015147b7b70951"
+    sha256 cellar: :any, arm64_ventura: "8f20895a4e5df248b8e196939ab1cab8658934e001743df6ae5a651c2cf2fe0a"
   end
 
   # libkrun, our only consumer, only supports Hypervisor.framework on arm64
