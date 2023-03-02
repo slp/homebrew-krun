6045c4c8d4394173f323edaadc6caeb2366610a8
Sergio Lopez <slp@sinrega.org>
2023-03-02 15:19:50 +0100

    libkrun: Update formula and bottle for v1.5.1

--- Formula/libkrun.rb
+++ Formula/libkrun.rb
@@ -1,13 +1,13 @@
 class Libkrun < Formula
   desc "Dynamic library providing KVM-based process isolation capabilities"
   homepage "https://github.com/containers/libkrun"
-  url "https://github.com/containers/libkrun/releases/download/v1.5.0/v1.5.0-with_macos_prebuilts.tar.gz"
-  sha256 "4f59d8cebcf1109695067234d0240f2a84f6398271756ef2e9636469b36c4182"
+  url "https://github.com/containers/libkrun/releases/download/v1.5.1/v1.5.1-with_macos_prebuilts.tar.gz"
+  sha256 "f213db9d9b436e255dc01aedc6de2767f400d8e16dba2d95b1095aa0e996c9ba"
   license "Apache-2.0"
 
   bottle do
     root_url "https://raw.githubusercontent.com/slp/homebrew-krun/master/bottles"
-    sha256 cellar: :any, arm64_ventura: "d864e2066b6afaf79e02414c1765073f761ea76f1de04f6d66bdc632026da0df"
+    sha256 cellar: :any, arm64_ventura: "16e011e8d435e1b400ef1db790094acf47986e60e7f29a9aff39e6de94f43929"
   end
 
   depends_on "rust" => :build
