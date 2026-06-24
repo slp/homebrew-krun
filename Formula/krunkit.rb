74489813974cd0577adbd009c7329026a1a20916
Sergio Lopez <slp@redhat.com>
2026-06-24 19:20:05 +0200

    krunkit: update to version 1.2.2
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- Formula/krunkit.rb
+++ Formula/krunkit.rb
@@ -1,15 +1,9 @@
 class Krunkit < Formula
   desc "CLI tool to start Linux KVM or macOS HVF VMs using the libkrun"
   homepage "https://github.com/containers/krunkit"
-  url "https://github.com/containers/krunkit/archive/refs/tags/v1.2.1.tar.gz"
-  sha256 "660d67d93d5c25caa996561b42ddca55848799673080bf5640ec1fba63d6260e"
+  url "https://github.com/containers/krunkit/archive/refs/tags/v1.2.2.tar.gz"
+  sha256 "355e73e42bc2f1d1087e15f417545646a03d020d332c319905e95d7849ef83f2"
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://raw.githubusercontent.com/slp/homebrew-krun/master/bottles"
-    sha256 cellar: :any, arm64_tahoe:   "fe04ffe1da4a41264734a9b730fef6ec386d5d87c105c2bea49ae04693447e9a"
-    sha256 cellar: :any, arm64_sequoia: "295e725a384fe593507b528b2c3583e2ba5e78bb439ac3d15afe60111b93c62f"
-  end
 
   depends_on "rust" => :build
   # We depend on libkrun, which only supports Hypervisor.framework on arm64
