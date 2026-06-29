3386aeca6683469863465c81c28b44382f54efcf
Sergio López <511389+slp@users.noreply.github.com>
2026-06-29 14:32:43 +0000

    krunkit: add 1.3.0 bottle.

--- Formula/krunkit.rb
+++ Formula/krunkit.rb
@@ -5,6 +5,12 @@ class Krunkit < Formula
   sha256 "ab2fa5e0c0b6d00b9aa083f4fb4703348dad98ce6ab18dfb559b769cb0d439a9"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/libkrun/homebrew-krun/releases/download/krunkit-1.3.0"
+    sha256 cellar: :any, arm64_tahoe:   "60591c9e756f5e235a19ad930e6b9a2fc1494fc359c0c1731815f26417ae5eb9"
+    sha256 cellar: :any, arm64_sequoia: "d77add6dac96e37c66af7ff75b6a6fb1a7132a70566c2e1822f1182e68aa0121"
+  end
+
   depends_on "rust" => :build
   # We depend on libkrun, which only supports Hypervisor.framework on arm64
   depends_on arch: :arm64
