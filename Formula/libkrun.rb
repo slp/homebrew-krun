26a2cfa9371f0359ce85c292c68fb380454b5ba8
Sergio Lopez <slp@redhat.com>
2026-07-03 12:52:47 +0200

    libkrun: update to version 1.19.4
    Closes #52.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>
    Signed-off-by: Sergio López <511389+slp@users.noreply.github.com>

--- Formula/libkrun.rb
+++ Formula/libkrun.rb
@@ -1,15 +1,9 @@
 class Libkrun < Formula
   desc "Dynamic library providing KVM-based process isolation capabilities"
   homepage "https://github.com/libkrun/libkrun"
-  url "https://github.com/containers/libkrun/archive/refs/tags/v1.19.3.tar.gz"
-  sha256 "955b0d948f1d1cf315c55ea92b55d5251928e6ec6f6aa6697cea95afccd4d2b0"
+  url "https://github.com/containers/libkrun/archive/refs/tags/v1.19.4.tar.gz"
+  sha256 "e8775fab2b460972a67ca6cd936296bb79cdb078d852d712a283cb290dd0b284"
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://github.com/libkrun/homebrew-krun/releases/download/libkrun-1.19.3"
-    sha256 cellar: :any, arm64_tahoe:   "1407b421ed98747334285a135f6bdb8e42daf6beac7076b2cfce1fb017e8179a"
-    sha256 cellar: :any, arm64_sequoia: "9bc73f8793f38fe2cc68c161dbf93f7abc5a5f9cc87da926d2cd8c2427de5f3a"
-  end
 
   depends_on "lld" => :build
   depends_on "rust" => :build
