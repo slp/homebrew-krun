af3b67ddaf0bbb2b9df26e2149098eca6ad60dc4
Sergio Lopez <slp@redhat.com>
2026-08-25 11:17:06 +0200

    krunvm: update to version 0.2.7
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- Formula/krunvm.rb
+++ Formula/krunvm.rb
@@ -1,15 +1,9 @@
 class Krunvm < Formula
   desc "Manage lightweight VMs created from OCI images"
   homepage "https://github.com/slp/krunvm"
-  url "https://github.com/containers/krunvm/archive/refs/tags/v0.2.6.tar.gz"
-  sha256 "9f13e938a10565cdd0d38bd49f8c7fc5be57245740bad2bb138eec8675d4ee3b"
+  url "https://github.com/containers/krunvm/archive/refs/tags/v0.2.7.tar.gz"
+  sha256 "1b2a54d670b09a8825a005184f7498a54535f86eb3d35850a37d4702da3121fc"
   license "Apache-2.0"
-
-  bottle do
-    root_url "https://raw.githubusercontent.com/slp/homebrew-krun/master/bottles"
-    sha256 cellar: :any, arm64_tahoe:   "ebb4d07cc32b2f7b0c17dde0db67ae315831877f0a472149ecf058fc536d4195"
-    sha256 cellar: :any, arm64_sequoia: "0bb8acfda1dcba4bc8e0fe893b677fd896e223c71f9f366a02f79939962d6700"
-  end
 
   depends_on "asciidoctor" => :build
   depends_on "rust" => :build
